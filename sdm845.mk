bc97c6fc9a6ee1011c78fc7e222ccafd436c1aed
Michael Bestas <mkbestas@lineageos.org>
2020-07-20 21:32:44 +0300

    sdm845-common: Remove android.hardware.contexthub
    Change-Id: I62bc93aee19c8070e4563825a07cfc48774e9b93

--- sdm845.mk
+++ sdm845.mk
@@ -124,11 +124,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
-# Context Hub
-PRODUCT_PACKAGES += \
-    android.hardware.contexthub@1.0-impl.generic \
-    android.hardware.contexthub@1.0-service
-
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
